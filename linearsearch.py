ef697f77c1179588ce0d5bfa559da1bb039c2dea
Narendra Reddy Yediginjala <narendra.yediginjala@gmail.com>
2019-11-14 15:05:27 -0800

    Update linearsearch.py

--- linearsearch.py
+++ linearsearch.py
@@ -1,11 +1,7 @@
-def binarysearch(l,x):
-	si = len(l)//2
+def linearsearch(l,x):
 	found = False
-	if si>=0 and si<len(l):
-		if l[si]==x:
+	for i in range(len(l)):
+		if l[i]==x:
 			found = True
-		elif l[si]>x:
-			found=binarysearch(l[:si],x)
-		else:
-			found=binarysearch(l[si+1:],x)
+			break
 	return found

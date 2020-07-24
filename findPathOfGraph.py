50b3fcf81488a034895d2d580f459a7f8f607109
Narendra Reddy Yediginjala <narendra.yediginjala@gmail.com>
2020-07-24 05:06:30 +0000

    Updated graph functions

--- findPathOfGraph.py
+++ findPathOfGraph.py
@@ -8,14 +8,51 @@ graph = {'A': ['B', 'C'],
 def findPathOfGraph(graph,start,end,path=[]):
   path = path +[start]
   if start==end:
-    return path
+    return [path]
   if not start in graph.keys():
     return none
+    path = []
   for node in graph[start]:
     if node not in path:
       new_path=findPathOfGraph(graph,node,end,path)
       if new_path:return new_path
   return none
 
+def find_all_paths(graph, start, end, path=[]):
+  path = path + [start]
+  if start == end:
+    return [path]
+  if not start in graph.keys():
+    return []
+  paths = []
+  for node in graph[start]:
+    if node not in path:
+      newpaths = find_all_paths(graph, node, end, path)
+      for newpath in newpaths:
+        paths.append(newpath)
+  return paths
+
+def find_shortest_paths(graph,start,end,path=[]):
+  path = path+[start]
+  if start==end:
+    return path
+  if not start in graph.keys():
+    return None
+  shortestpath=None
+  for node in graph[start]:
+    if node not in path:
+      newpath = find_shortest_paths(graph,node,end,path)
+      if newpath:
+        if (not shortestpath) or (len(newpath)<len(shortestpath)):
+          shortestpath=newpath
+  return shortestpath
+
+
 p=findPathOfGraph(graph,'A','D')
-print(p)
+print("first path found=",p)
+
+np=find_all_paths(graph,'A','D')
+print("all paths=",np)
+
+sp=find_shortest_paths(graph,'A','D')
+print("shortest path=",sp)

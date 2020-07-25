b27e5cb1da6798235e7c4f17e7acc6d4b50135f9
Narendra Reddy Yediginjala <narendra.yediginjala@gmail.com>
2020-07-25 02:23:27 +0000

    Added linked list, binary search tree

---
+++ binarySearchTree.py
@@ -0,0 +1,32 @@
+class Node:
+  def __init__(self,data,parent):
+    self.data=data
+    self.leftChild=None
+    self.rightChild=None
+    self.parent=parent
+
+class bst:
+  def __init__(self):
+    self.root=None
+
+  def insert(self,data):
+    if self.root is None:
+      node = Node(data,None)
+      self.root=node
+    else:
+      self.insert_node(data,self.root)
+
+  def insert_node(self,data,node):
+    if data <node.data:
+      if node.leftChild:
+        self.insert_node(data,node.leftChild)
+      else:
+        node.leftChild=Node(data,node)
+    else:
+      if node.rightChild:
+        self.insert_node(data,node.rightChild)
+      else:
+        node.rightChild=Node(data,node)
+b = bst()
+b.insert(30)
+b.insert(55)
--- linkedlistPython.py
+++ linkedlistPython.py
@@ -1,40 +1,14 @@
 class Node:
-  def __init__(self,value):
-    self.value = value
-    self.Next = None
-
-class linked_list:
+  def __init__(self,data):
+    self.data=data
+    self.Next=None
+class LinkedList:
   def __init__(self):
-    self.head=Node()
-  def append(self,data):
-    new_node = Node(data)
-    curr = self.head
-    while curr.Next!=None:
-      curr = curr.Next
-    curr.Next=new_node
-  def length(self):
-    total=0
-    curr = self.head
-    while curr.Next!=None:
-      total+=1
-      curr = curr.Next
-    return total
-  def display(self):
-    elements = []
-    curr = self.head
-    while curr.Next!=None:
-      elements = elements.append(curr.value)
-      curr=curr.Next
-    print(elements)
-  def getItem(self,index):
-    if index >= self.length or index<0:
-      print("Error: Index out of range")
-      return None
-    curr_index = 0
-    curr_node = self.head
-    while True:
-      if curr_index ==index:
-        return curr_node.value
-      curr_node = curr_node.Next
-      curr_index+=1
-    
+    self.head=None
+    self.numOfNodes=0
+  def insert(self,data):
+    node=Node(data)
+    if not self.head:
+      self.head=node
+l=LinkedList()
+l.insert(10)
